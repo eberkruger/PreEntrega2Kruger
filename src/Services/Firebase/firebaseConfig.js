52163bdcd1cf4e92f4cdfa979107e1a008a05c02
eberkruger <eberkruger@hotmail.com.ar>
2023-04-11 20:26:43 -0300

    Instalación Firebase

---
+++ src/Services/Firebase/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNaf7mhkfvsHwkzB6t_bXk_ilEDGG4Whc",
+  authDomain: "backendtechzone.firebaseapp.com",
+  projectId: "backendtechzone",
+  storageBucket: "backendtechzone.appspot.com",
+  messagingSenderId: "15909136858",
+  appId: "1:15909136858:web:2a88ea5ee03ad70342f8e3"
+};
+
+// Iniciarlizar Firebase
+const app = initializeApp(firebaseConfig);
+// Inicializar Firestore
+const db = getFirestore(app)
